232dec81d64ff3192b2feb0417b7aed4e2700a07
ppoox <ppoox@naver.com>
2022-08-11 00:27:06 +0900

    chore: 설정 수정
    - eslint 수정
    - warning 처리
    - 경로 alias 추가
    - mixins.scss 전역 처리

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,29 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '*': path.join(__dirname, '/'),
+        '@': path.join(__dirname, 'src/'),
+        '@styles': path.join(__dirname, 'src/assets/styles'),
+        '@images': path.join(__dirname, 'src/assets/images'),
+        '@utils': path.join(__dirname, 'src/utils'),
+        '@assets': path.join(__dirname, 'src/assets'),
+        '@components': path.join(__dirname, 'src/components'),
+        '@constants': path.join(__dirname, 'src/constants'),
+        '@api': path.join(__dirname, 'src/api'),
+        '@routes': path.join(__dirname, 'src/router/routes'),
+        '@pages': path.join(__dirname, 'src/pages')
+      }
+    }
+  },  
   css: {
     loaderOptions: {
       sass: {
-
+        additionalData: `
+          @import "@/assets/styles/mixins.scss";
+        `
       }
     }
   },
