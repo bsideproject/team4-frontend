f7e9d90d2bee7b09d35fa7a04dcd3aff264b5d7e
ppoox <ppoox@naver.com>
2022-08-11 21:08:47 +0900

    fix: layout 수정
    - header 상단바와 GNB로 분리
    - header, gnb scss 분리
    - div => header, section tag로 수정
    - 아이콘 동기화

---
+++ src/components/common/Gnb.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="gnb">
+    <div class="gnb__category">
+      <button class="category__item">체크리스트</button>
+      <button class="category__item">양육일지</button>
+      <button class="category__item">건강일지</button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+@import '@styles/common/gnb.scss';
+</style>
--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,21 +1,17 @@
 <template>
-  <div class="header section">
+  <header class="header section">
     <div class="header-main">
-      <a class="header-main__logo">logo</a>
-      <router-link to="/setting" class="header-main__menu">menu</router-link>
+      <a class="header-main__logo"></a>
+      <span>펫하루</span>
+      <router-link to="/setting" class="header-main__menu" />
     </div>
-    <div class="header-sub">
-      <div class="header-sub__tabMenu">
-        <button class="tabMenu__item">체크리스트</button>
-        <button class="tabMenu__item">양육일지</button>
-      </div>
-    </div>
-  </div>
+  </header>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import '@styles/common/header.scss';
 </style>
