efd89aa4580c23f48c621280763b9202e6b52a33
jinri <jinri@dfluid.kr>
2022-08-16 14:16:58 +0900

    fix: 컴포넌트 내용 수정, 라우트 수정

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,16 @@
 <script>
 import CompHeader from '@/components/common/Header.vue'
 import CompFooter from '@/components/common/Footer.vue'
+import PetProfile from '@/components/common/PetProfile.vue'
+import WeeklyCalendar from '@/components/common/WeeklyCalendar.vue'
 
 export default {
   name: 'App',
   components: {
     CompHeader,
     CompFooter,
+    PetProfile,
+    WeeklyCalendar
   },
 }
 </script>
