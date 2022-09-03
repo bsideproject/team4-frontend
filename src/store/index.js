a92f33a7b696cd3f144fdf142ca8ff04bf252751
ppoox <ppoox@naver.com>
2022-09-03 17:41:46 +0900

    feat: 퀵기록관리 퍼블리싱

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,13 @@ import { createStore } from 'vuex'
 import { module as headerStore } from './modules/common/headerStore'
 import { module as groupStore } from './modules/group/groupStore'
 import { module as petStore } from './modules/pet/petStore'
+import { module as checklistStore } from './modules/checklist/checklistStore'
 
 export default createStore({
   modules: {
     headerStore,
     groupStore,
     petStore,
+    checklistStore
   }
 })
