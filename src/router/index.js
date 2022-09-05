acd9deac45bedcb726a4ea821e240f0b9fdbf308
ppoox <ppoox@naver.com>
2022-09-05 22:53:58 +0900

    fix: router auth 주석 해제

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import routes from './routes/index.js'
 import store from '../store/index.js'
-// import { getToken } from '@utils/login/index.js'
-// import ROUTE from '@constants/route.json'
+import { getToken } from '@utils/login/index.js'
+import ROUTE from '@constants/route.json'
 
 export const router = createRouter({
   history: createWebHistory(),
@@ -12,8 +12,7 @@ export const router = createRouter({
 router.beforeEach((to, from, next) => {
   console.log('beforeEach', to)
 
-  // const { auth, title, headerType } = to.meta
-  const { title, headerType } = to.meta
+  const { auth, title, headerType } = to.meta
   store.commit('headerStore/setTitle', title)
   store.commit('headerStore/setHeaderType', headerType)
 
@@ -21,17 +20,17 @@ router.beforeEach((to, from, next) => {
   const serviceName = browserTitle.split('-')[0]?.trim()
   document.title = title ? `${serviceName} - ${title}` : serviceName
 
-  // if (auth) {
-  //   const token = getToken()
-  //   if (token) {
-  //     /** 토큰 유효성 API 호출 */
-  //     next()
-  //   } else {
-  //     /** 로그인 페이지로 이동 */
-  //     next({ name: ROUTE.Login })
-  //   }
-  // } else {
-  //   next()
-  // }
+  if (auth) {
+    const token = getToken()
+    if (token) {
+      /** 토큰 유효성 API 호출 */
+      next()
+    } else {
+      /** 로그인 페이지로 이동 */
+      next({ name: ROUTE.Login })
+    }
+  } else {
+    next()
+  }
   next()
 })
