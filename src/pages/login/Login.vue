064b3a098d29c5ff2d5942a76e056caf65fb1db1
ppoox <ppoox@naver.com>
2022-10-07 22:35:09 +0900

    fix: 로그인 약관 추가

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -11,6 +11,21 @@
         구글 계정으로 로그인
       </button>
     </article>
+    <article class="login__terms">
+      <div>
+        <p class="terms__notice">
+          회원가입 하는 경우
+          <a
+            href="https://fuzzy-scarer-071.notion.site/b9ece4c53a84451fa92fb3776baba1f6"
+            >서비스 이용약관</a
+          >과<br />
+          <a
+            href="https://fuzzy-scarer-071.notion.site/6e25778fac994bf49d41e3bbda89a0d3"
+            >개인정보처리방침</a
+          >에 동의하는 것으로 간주합니다.
+        </p>
+      </div>
+    </article>
   </section>
 </template>
 
